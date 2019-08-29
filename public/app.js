82dcc260bca2f0ef7db90a1d7e6c0053a34d20dc
Yating(Esther) Su <yaringsu@foxmail.com>
2019-08-28 20:10:51 -0400

    Add button to each news and make it able to open modal Onclick

--- public/app.js
+++ public/app.js
@@ -7,9 +7,18 @@ $("#scraptNYT").on("click", function(){
         console.log(data);
         for(let i = 0; i< 100; i++){
             $("#articleList").append(`
-            <h2><a href = ${data[i].link}>${data[i].title}<a></h2>
-            <p>${data[i].discription}</p>
+            <div class="card" style="width: 18rem;">
+            <div class="card-body">
+            <h6 class="card-title"><a href = ${data[i].link}>${data[i].title}<a></h6>
+            <p class="card-text">${data[i].discription}</p>
+            <button class="btn btn-warning btn-sm addNote" data-toggle="modal" data-target="#basicModal" data-id=${data[i]._id}>Add Notes</button>
+            <div>
+            <div>
         `)
         }
     })
 });
+
+$("#articleList").on("click",".addNote", function(){
+    console.log($(this).attr("data-id"));
+})
