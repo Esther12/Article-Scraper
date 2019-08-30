131e22611165abeebec476baa8bbb6b19213e9f2
Yating(Esther) Su <yaringsu@foxmail.com>
2019-08-30 15:55:23 -0400

    Push to Heroku

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.use(express.static("public"));
 //Connecting to MongoDB
 //mongoose.connect(process.env.PORT || "mongodb://user:ro0tro0t@ds355357.mlab.com:55357/heroku_cs6dl5ll",{ useNewUrlParser: true });
 
-//var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:ro0tro0t@ds355357.mlab.com:55357/heroku_cs6dl5ll";
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytscraper";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:ro0tro0t@ds355357.mlab.com:55357/heroku_cs6dl5ll";
+//var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytscraper";
 mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
 
 console.log("\n***********************************\n" +
